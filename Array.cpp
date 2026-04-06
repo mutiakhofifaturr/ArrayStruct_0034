0ee5fef373782144d913de25bba3ef5ddf4a714c
mimyutt <mutiakhofifaturr@gmail.com>
2026-04-06 11:00:00 +0700

    menambahkan isi data melalui perulangan dan data dengan pengulangan

--- Array.cpp
+++ Array.cpp
@@ -34,6 +34,18 @@ int main() {
     cout << "data keempat = " << datamu[3] << endl;
     cout << "data kelima = " << datamu[4] << endl;
 
+    // mengisi data melalui perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Isikan data ke-" << (i + 1) << " : ";
+        cin >> datamu[i];
+    }
+
+    // menampilkan data dengan perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " : " << datamu[i] << endl;
+    }
 
     return 0;
 }
