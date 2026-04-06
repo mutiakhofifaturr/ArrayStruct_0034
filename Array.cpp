c62c696c5786269254037110a1547305f92f01ff
mimyutt <mutiakhofifaturr@gmail.com>
2026-04-06 10:47:05 +0700

    menambah isi kedua setelah dirubah

--- Array.cpp
+++ Array.cpp
@@ -12,4 +12,6 @@ int main() {
     cout << "Isi data kedua : " << dataku[1] << endl;
   
   // mengisi array
+   dataku[1] = 200;
+   cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
 }
