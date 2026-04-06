cf01388814ca3255ee079eb751bd3becd52d8f05
mimyutt <mutiakhofifaturr@gmail.com>
2026-04-06 10:42:44 +0700

    menambahkan isi array

---
+++ Array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // deklarasi array disertai inisialisasi
+  int dataku[5] = {10, 20, 30, 40, 50};
+
+  // deklarasi tanpa inisialisasi
+    int datamu[5];
+
+  // cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku[1] << endl;
+  
+  // mengisi array
+}
