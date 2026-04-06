a3d68983c5064832e28e04959195966a5cb78749
mimyutt <mutiakhofifaturr@gmail.com>
2026-04-06 11:19:58 +0700

    menambahkan deklarasi variable struct

--- Struct1.cpp
+++ Struct1.cpp
@@ -8,3 +8,20 @@ struct mahasiswa
     int umur;
 };
 
+int main()
+{
+    //Deklarasi variabel struct
+    mahasiswa mhs;
+    //Mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamt : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+
+    cout << "Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
